72ba17014ec0e126dc23914ee32f84c2035f54b9
Albert <albert.gomes@liferay.com>
2025-05-05 22:28:34 -0300

    Remove classes from main app

--- src/main/java/com/PooCourseTasksApplication.java
+++ src/main/java/com/PooCourseTasksApplication.java
@@ -1,29 +1,9 @@
-package com.task;
-
-import com.task.modules.Example;
-import com.task.modules.one.MediaAgeExample;
-import com.task.modules.one.SequentialStructureExample;
-import com.task.modules.one.TradeExample;
-import com.task.modules.two.IncomeTax;
+package com;
 
 public class PooCourseTasksApplication {
 
     public static void main(String[] args) {
-        Example example = new SequentialStructureExample();
 
-        example.execute();
-
-        Example example1 = new MediaAgeExample();
-
-        example1.execute();
-
-        Example example2 = new TradeExample();
-
-        example2.execute();
-
-        Example example3 = new IncomeTax();
-
-        example3.execute();
     }
 
 }
